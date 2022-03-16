1498e4fda6163dda9da9de244626a176294b9bfb
vigno88 <nathan.alix88@gmail.com>
2022-03-16 19:56:11 -0400

    Added demo

--- main.cpp
+++ main.cpp
@@ -17,7 +17,7 @@ Servo gripper(PE_13);
 
 int main() {
   enable.write(0);
-
+  
   base.attach(0);
   shoulder.attach(0);
   elbow.attach(1);
@@ -25,23 +25,50 @@ int main() {
   wrist_vert.attach(0);
   gripper.attach(0);
   
-  base.write(0);
-  shoulder.write(0);    
-  elbow.write(5);       // center
-  wrist_rot.write(0);
-  wrist_vert.write(-3);
-  gripper.write(0);
+  base.write(0);        // center  0  max 110   min -100
+  shoulder.write(0);    // center  0  max 50    min -50
+  elbow.write(5);       // center  5  max 100   min -100
+  wrist_rot.write(-5);  // center -5  max 100   min -100
+  wrist_vert.write(-3); // center -3  max 110   min -100
+  gripper.write(-20);     // center -20 closed 10 open -50
 
   enable.write(1);
+  ThisThread::sleep_for(1s);
   
-  while (1) {
-    base.write(0);
-    shoulder.write(0);
-    elbow.write(5);
-    wrist_rot.write(0);
-    wrist_vert.write(-3);
-    gripper.write(0);
+  // base
+  base.write(-30);
+  ThisThread::sleep_for(500ms);
+  base.write(30);
+  ThisThread::sleep_for(500ms);
+  base.write(0);
 
-    ThisThread::sleep_for(200ms);
-  }
+  ThisThread::sleep_for(1s);
+   
+  // 3 joints
+  shoulder.write(-20);
+  elbow.write(25);
+  wrist_vert.write(-23);
+  ThisThread::sleep_for(500ms);
+  shoulder.write(20);
+  elbow.write(-15);
+  wrist_vert.write(17);
+  ThisThread::sleep_for(500ms);
+  shoulder.write(0);
+  elbow.write(5);
+  wrist_vert.write(-3);
+  ThisThread::sleep_for(500ms);
+
+  ThisThread::sleep_for(1s);
+  
+  // wrist
+  wrist_rot.write(100);
+  ThisThread::sleep_for(500ms);
+  wrist_rot.write(-5);
+
+  ThisThread::sleep_for(1s);
+  
+  // gripper
+  gripper.write(-50);
+  ThisThread::sleep_for(500ms);
+  gripper.write(-20);
 }
